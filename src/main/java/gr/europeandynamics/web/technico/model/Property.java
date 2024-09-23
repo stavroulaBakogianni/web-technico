f596095ca8ea1899a18a6d2012b1c900f0f301df
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-09-23 22:17:31 +0300

    Create Property.java

---
+++ src/main/java/gr/europeandynamics/web/technico/model/Property.java
@@ -0,0 +1,66 @@
+package gr.europeandynamics.web.technico.model;
+
+import gr.technico.technikon.model.User;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Property implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(min = 20, max = 20, message = "E9 must contain 20 characters.")
+    @NotNull
+    @Column(unique = true)
+    private String e9;
+
+    @Column(name = "property_address", length = 50)
+    private String propertyAddress;
+
+    @Digits(integer = 4, fraction = 0)
+    @Column(name = "construction_year")
+    private int constructionYear;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "property_type", nullable = false)
+    private PropertyType propertyType;
+
+    @NotNull
+    private boolean isDeleted = false;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    
+    @OneToMany(mappedBy = "property", cascade = CascadeType.ALL)
+    private List<Repair> repairs;
+
+    @Override
+    public String toString() {
+    return "Property{" + "id=" + id + ", e9=" + e9 + ", propertyAddress=" + propertyAddress + ", constructionYear=" + constructionYear + ", propertyType=" + propertyType + ", owner=" + user.getVat() + ", isDeleted=" + isDeleted + '}';
+}
+}
