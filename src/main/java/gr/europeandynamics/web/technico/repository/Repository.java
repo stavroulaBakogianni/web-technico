be73e25638616cd45c85a9ebc9204ddd54504983
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-09-25 00:36:21 +0300

    Create Repository.java

---
+++ src/main/java/gr/europeandynamics/web/technico/repository/Repository.java
@@ -0,0 +1,15 @@
+package gr.europeandynamics.web.technico.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Repository<T, K> {
+
+    Optional<T> save(T t);
+
+    Optional<T> findById(K id);
+
+    List<T> findAll();
+
+    boolean deleteById(K id);
+}
