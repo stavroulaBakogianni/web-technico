c7ad8ff5750c74bb8129415c3d82d942d49e4cd4
Stavroula Bakogianni <stauroulabak@gmail.com>
2024-09-25 00:36:37 +0300

    Create PropertyRepositoryImpl.java

---
+++ src/main/java/gr/europeandynamics/web/technico/repository/PropertyRepositoryImpl.java
@@ -0,0 +1,95 @@
+package gr.europeandynamics.web.technico.repository;
+
+import gr.europeandynamics.web.technico.model.Property;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequestScoped
+@NoArgsConstructor
+public class PropertyRepositoryImpl implements Repository<Property, Long> {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    @Transactional
+    public Optional<Property> save(Property property) {
+        try {
+            entityManager.persist(property);
+            return Optional.of(property);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return Optional.empty();
+        }
+
+    }
+
+    @Override
+    public Optional<Property> findById(Long id) {
+        Property property;
+        try {
+            property = entityManager.find(getEntityClass(), id);
+            return Optional.of(property);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<Property> findAll() {
+        TypedQuery<Property> query
+                = entityManager.createQuery("from " + getEntityClassName(), getEntityClass());
+        return query.getResultList();
+    }
+
+    @Transactional
+    @Override
+    public boolean deleteById(Long id) {
+        try {
+            Property property = entityManager.find(getEntityClass(), id);
+            if (property != null) {
+                entityManager.remove(property);
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return false;
+        }
+    }
+
+    public Optional<Property> findPropertyByE9(String e9) {
+        TypedQuery<Property> query
+                = entityManager.createQuery("from " + getEntityClassName()
+                        + " where e9 = :e9 ",
+                        getEntityClass())
+                        .setParameter("e9", e9);
+        return query.getResultStream().findFirst();
+    }
+
+    public List<Property> findPropertiesByVAT(String vat) {
+        TypedQuery<Property> query
+                = entityManager.createQuery("from " + getEntityClassName()
+                        + " where user_vat = :vat ",
+                        getEntityClass())
+                        .setParameter("vat", vat);
+        return query.getResultList();
+    }
+
+    private Class<Property> getEntityClass() {
+        return Property.class;
+    }
+
+    private String getEntityClassName() {
+        return Property.class.getName();
+    }
+}
